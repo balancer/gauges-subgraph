f2eec79a5e3fbe0c3c10837bce5a366fd5a3d742
Fábio Mendes <ms.fabiomendes@gmail.com>
2022-08-23 10:05:30 -0300

    fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,15 @@
 module.exports = {
-  extends: "airbnb-base-typescript-prettier",
   parser: "@typescript-eslint/parser",
   parserOptions: {
-      "project": "./tsconfig.json",
-      "tsconfigRootDir": __dirname,
-      "sourceType": "module"
+    "project": "./tsconfig.json",
+    "tsconfigRootDir": __dirname,
+    "sourceType": "module"
   },
+  plugins: ["@typescript-eslint", "prettier", "mocha-no-only"],
   env: {
     es6: true,
   },
   rules: {
-    "import/extensions": [
-      "error",
-      "ignorePackages",
-      {
-        "ts": "never"
-      }
-    ],
     "import/prefer-default-export": "off",
     "prefer-destructuring": "off",
     "prefer-template": "off",
@@ -24,5 +17,6 @@ module.exports = {
     "prefer-const": "off",
     "@typescript-eslint/no-shadow": "off",
     "lines-between-class-members": "off"
-  }
-}
+  },
+};
+
