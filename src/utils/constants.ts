8202be70d928411dcb9f8293608142aadeaa4b50
Fábio Mendes <ms.fabiomendes@gmail.com>
2022-08-26 09:59:17 -0300

    create helpers for factories

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -31,3 +31,29 @@ export const CONTROLLER_ADDRESS = forNetwork(controllerAddressByNetwork, network
 export const ARBITRUM_ROOT_GAUGE_FACTORY = Address.fromString('0xad901309d9e9DbC5Df19c84f729f429F0189a633');
 export const OPTIMISM_ROOT_GAUGE_FACTORY = Address.fromString('0x3083A1C455ff38d39e58Dbac5040f465cF73C5c8');
 export const POLYGON_ROOT_GAUGE_FACTORY = Address.fromString('0x4C4287b07d293E361281bCeEe8715c8CDeB64E34');
+
+export const MAINNET_GAUGE_V2_FACTORY = Address.fromString('0x3b8cA519122CdD8efb272b0D3085453404B25bD0');
+export const ARBITRUM_ROOT_GAUGE_V2_FACTORY = Address.fromString('0x1c99324EDC771c82A0DCCB780CC7DDA0045E50e7');
+export const OPTIMISM_ROOT_GAUGE_V2_FACTORY = Address.fromString('0x866D4B65694c66fbFD15Dd6fa933D0A6b3940A36');
+export const POLYGON_ROOT_GAUGE_V2_FACTORY = Address.fromString('0xa98Bce70c92aD2ef3288dbcd659bC0d6b62f8F13');
+
+export function isArbitrumFactory(factory: Address): boolean {
+  return [ARBITRUM_ROOT_GAUGE_FACTORY, ARBITRUM_ROOT_GAUGE_V2_FACTORY].includes(factory);
+}
+
+export function isOptimismFactory(factory: Address): boolean {
+  return [OPTIMISM_ROOT_GAUGE_FACTORY, OPTIMISM_ROOT_GAUGE_V2_FACTORY].includes(factory);
+}
+
+export function isPolygonFactory(factory: Address): boolean {
+  return [POLYGON_ROOT_GAUGE_FACTORY, POLYGON_ROOT_GAUGE_V2_FACTORY].includes(factory);
+}
+
+export function isV2Factory(factory: Address): boolean {
+  return [
+    MAINNET_GAUGE_V2_FACTORY,
+    ARBITRUM_ROOT_GAUGE_V2_FACTORY,
+    OPTIMISM_ROOT_GAUGE_V2_FACTORY,
+    POLYGON_ROOT_GAUGE_V2_FACTORY,
+  ].includes(factory);
+}
