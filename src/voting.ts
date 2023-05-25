65b75e4e58e79c817244265b4beb362118568a3f
Markus Buhatem Koch <markusbkoch@gmail.com>
2023-05-25 17:08:17 +0100

    fix casting

--- src/voting.ts
+++ src/voting.ts
@@ -120,8 +120,7 @@ export function handleUserBalFromChain(event: UserBalFromChain): void {
   votingShare.bias = scaleDownBPT(event.params.userPoint.bias);
   votingShare.timestamp = event.params.userPoint.ts.toI32();
   votingShare.unlockTime = event.params.userPoint.ts
-    .plus(event.params.userPoint.bias.div(event.params.userPoint.slope))
-    .toI32();
+    .plus(event.params.userPoint.bias.div(event.params.userPoint.slope));
 
   votingShare.save();
 }
